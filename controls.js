7cfe79eef5aee4552343140e0d4b77565ece19d2
Andrew Ewen <Andrew Ewen>
2016-02-08 01:16:08 +0000

    Add mouse control support. Make pretty graph thing.

--- controls.js
+++ controls.js
@@ -1,14 +1,24 @@
 "use strict";
 var Controls = (function () {
+  // Handy object for looking up names of keys.
   const KEYS = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40,
-                 SPACE: 32, E: 69, W: 87, A: 65, S: 83, D: 68 }
+                 SPACE: 32, E: 69, W: 87, A: 65, S: 83, D: 68 };
+  // Map object that will store state of all keys that have been pressed.
+  let keyStates = new Map();
+  // Array containing all keys we should watch.
   let watchKeyCodes = [];
   for (var k in KEYS) {
     if (KEYS.hasOwnProperty(k)) watchKeyCodes.push(KEYS[k]);
   }
-  let keyStates = new Map();
 
+  let mouseRelative;
+  let mousePos = { x: 0, y: 0 };
+  let mousePressed = false;
+  let mousePressedTime = 0;
+
+  // Returns whether or not the key is in the watchKeyCodes array.
   function isTracked (keyCode) { return watchKeyCodes.indexOf(keyCode) != -1 };
+  // Updates the keyStates with a new keyState as long as it is a tracked key.
   function setKeyPressed (keyCode, pressed) {
     if (isTracked(keyCode)) {
       if (!keyStates.get(keyCode)) {
@@ -18,44 +28,100 @@ var Controls = (function () {
     }
   }
 
+  // Updates the key states when a button is pressed down.
   window.addEventListener("keydown", function (e) {
     if (isTracked(e.keyCode)) e.preventDefault();
     setKeyPressed(e.keyCode, true);
   });
+  // Updates the key states when a button is released
   window.addEventListener("keyup", function (e) {
     setKeyPressed(e.keyCode, false);
   });
 
-  function isPressed (keyCode) {
+  // Returns whether or not the requested key is currently pressed.
+  function isKeyPressed (keyCode) {
     let keyState = keyStates.get(keyCode);
     return keyState && keyState.pressed;
   }
 
-  function timePressed (keyCode) {
+  // Queries how long the key has been pressed for since the last time
+  // reset was called.
+  function timeKeyPressed (keyCode) {
     let keyState = keyStates.get(keyCode);
     return keyState ? keyState.time : 0;
   }
 
-  function update(deltaTime) {
+  // Function called when the mouse is pressed on the mouseRelative object.
+  function onMouseDown (e) {
+    e.preventDefault();
+    mousePressed = true;
+  }
+  // Function called when the mouse is released or leaves the mouseRelative
+  // object.
+  function onMouseUp (e) { mousePressed = false; }
+  // Function called when the mouse moves on the mouseRelative object.
+  function onMouseMove (e) {
+    let rect = mouseRelative.getBoundingClientRect();
+    mousePos.x = e.clientX - rect.left;
+    mousePos.y = e.clientY - rect.top;
+  }
+
+  // Sets position for mouse to be detected on and for the mouse to be
+  // positioned relative to.
+  function setMouseRelative (object) {
+    if (object !== mouseRelative) {
+      mousePos = { x: 0, y: 0 };
+      mousePressed = false;
+
+      if (mouseRelative) {
+        mouseRelative.removeEventListener("mousedown", onMouseDown);
+        mouseRelative.removeEventListener("mouseup", onMouseUp);
+        mouseRelative.removeEventListener("mouseleave", onMouseUp);
+        mouseRelative.removeEventListener("mousemove", onMouseMove);
+      }
+      mouseRelative = object;
+      mouseRelative.addEventListener("mousedown", onMouseDown);
+      mouseRelative.addEventListener("mouseup", onMouseUp);
+      mouseRelative.addEventListener("mouseleave", onMouseUp);
+      mouseRelative.addEventListener("mousemove", onMouseMove);
+    }
+  }
+
+  function isMousePressed () { return mousePressed; }
+  function getMousePosition () { return mousePos; }
+
+  // Increments every pressed key and the mouse's time value.
+  function updateTimes(deltaTime) {
     for (let state of keyStates.values()) {
       if (state.pressed) {
         state.time += deltaTime;
       }
     }
+
+    if (mousePressed) mousePressedTime += deltaTime;
   }
 
-  function reset () {
+  // Reset every keyState and the mouse's time value.
+  function resetTimes () {
     for (let state of keyStates.values()) {
       state.time = 0;
     }
+
+    mousePressedTime = 0;
   }
 
+  // Return the things we want to reveal to the world.
   return {
+    // Public keyboard stuff.
     KEYS: KEYS,
-
-    isPressed: isPressed,
-    timePressed: timePressed,
-    update: update,
-    reset: reset
+    isKeyPressed: isKeyPressed,
+    timeKeyPressed: timeKeyPressed,
+    // Public mouse stuff.
+    setMouseRelative: setMouseRelative,
+    isMousePressed: isMousePressed,
+    getMousePosition: getMousePosition,
+    // General control stuff.
+    updateTimes: updateTimes,
+    resetTimes: resetTimes
   }
 })();
--- main.js
+++ main.js
@@ -4,37 +4,40 @@ window.onload = function () {
   const ctx = canvas.getContext("2d");
   const size = { x: canvas.width, y: canvas.height };
 
+  Controls.setMouseRelative(canvas);
+
   ctx.fillStyle = "#4b0";
-  let x = 0;
+
+  const DATA_POINTS = 64;
+  let data = [];
+  for (let i = 0; i < DATA_POINTS; i++) data.push(0);
+  setInterval(function () {
+      let index = Math.floor(Math.random() * data.length);
+      data[index] = 1
+    }, 200)
 
   let lastTime = window.performance.now();
+
   function loop () {
     const deltaTime = (window.performance.now() - lastTime) * 0.001;
     lastTime = window.performance.now();
 
     ctx.clearRect(0, 0, size.x, size.y);
 
-    Controls.update(deltaTime);
-    if (Controls.isPressed(Controls.KEYS.SPACE)) Controls.reset();
+    Controls.updateTimes(deltaTime);
 
-    //let times = [ Controls.timePressed(Controls.KEYS.A),
-    //              Controls.timePressed(Controls.KEYS.S),
-    //              Controls.timePressed(Controls.KEYS.D) ];
-    if(Controls.isPressed(Controls.KEYS.LEFT)) x-=10 * deltaTime;
-    if(Controls.isPressed(Controls.KEYS.RIGHT)) x+=10 * deltaTime;
-    let times = [];
-    for (let i = 0; i < 100; i++) times.push((Math.sin(x+0.03*i)+1)/2);
-    for (let i = 0; i < times.length; i++) {
-      ctx.fillStyle = tinycolor({h: (i/times.length) * 360, s: 100, v: 100}).toHexString();
-      ctx.fillRect(i * (size.x / times.length),
-                   size.y - (times[i] * size.y),
-                   size.x / times.length,
-                   times[i] * size.y);
+    data.forEach(function (v, i, a) {a[i] -= 0.6 * deltaTime});
+    if(Controls.isMousePressed()) data[Math.floor(Controls.getMousePosition().x / size.x * data.length)] = 1;
+
+    for (let i = 0; i < data.length; i++) {
+      ctx.fillStyle = tinycolor({h: data[i] * 360, s: 40, v: 100}).toHexString();
+      ctx.fillRect(i * (size.x / data.length),
+                   size.y - (data[i] * size.y),
+                   size.x / data.length,
+                   data[i] * size.y);
     }
 
     requestAnimationFrame(loop);
   }
   requestAnimationFrame(loop);
-
-  // setInterval(function () { Controls.reset(); }, 1000);
 }
